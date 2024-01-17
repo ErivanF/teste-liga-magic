38efbd50dcd74ae43c8edd620a06e7faf2a813e2
Erivan <erivan.c39@gmail.com>
2024-01-16 22:24:07 -0300

    adicionada imagem principal

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const cardData = {
+  imageMain: "assets/images/placeholder_large.png",
+  imageSecondary: [
+    "assets/images/carousel1.png",
+    "assets/images/carousel1.png",
+    "assets/images/carousel1.png",
+  ],
+};
+
+const mainImage = document.getElementById("mainImage");
+console.log(mainImage);
+mainImage.src = cardData.imageMain;
